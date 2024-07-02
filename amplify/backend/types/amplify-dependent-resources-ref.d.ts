c2f29c8d92b3730e7e7e6661e87f10e93a090a7b
ryo izuta <r.izuta@mplpl.com>
2024-07-02 18:33:00 +0900

    ✨ サインインの実装

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "awstutorialreactf0c06552": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import {Amplify} from "aws-amplify";
+import {Button, withAuthenticator} from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
-function App() {
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
+function App({signOut, user}) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>HEllo AWS</h2>
+        {user ? (
+          <>
+            <h3>私は権限を持っています: {user.username}</h3>
+            <Button onClick={signOut}>サインアウト</Button>
+          </>
+        ):(
+          <>
+          <h3>私には権限がありません。</h3>
+          </>
+        )}
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
